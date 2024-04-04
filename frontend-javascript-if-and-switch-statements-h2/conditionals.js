e9d745509456aa7929f32dde0bf3c61ccb7caab0
AnneKluytmans <annekluytmans1@gmail.com>
2024-04-04 12:21:39 +0200

    Finished assignment about if- and switch-statements

---
+++ frontend-javascript-if-and-switch-statements-h2/conditionals.js
@@ -0,0 +1,61 @@
+// ==========================================
+// 0. Voorbeeldvraag: declareer een variabele met daarin de vergelijking of 2 gelijk is aan 2.                // geeft true wanneer gelogd
+// ==========================================
+
+const equalNumbers = 2 === 2;
+console.log('Is 2 gelijk aan 2?', equalNumbers);
+
+// ==========================================
+// 1. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 groter dan 20?" in op   // geeft false wanneer gelogd
+// ==========================================
+
+const compareNumbers1 = 10 > 20;
+console.log("Is 10 groter dan 20?", compareNumbers1);
+
+// ==========================================
+// 2. Declareer een variabele en sla daar de uitkomst van de vergelijking "Is 10 ongelijk aan 11?" in op    // geeft true wanneer gelogd
+// ==========================================
+
+const compareNumbers2 = 10 != 11;
+console.log("Is 10 ongelijk aan 11?", compareNumbers2);
+
+// ==========================================
+// 3. Declareer een variabele en sla daar in op of de woorden 'zoet' en 'sappig' hetzelfde zijn   // geeft false wanneer gelogd
+// ==========================================
+
+const compareWords = 'zoet' == 'sappig';
+console.log("Is zoet hetzelfde woord als sappig?", compareWords);
+
+// ==========================================
+// 4a. Declareer een variabele en sla daar het rapportcijfer 9 in op.
+// 4b. schrijf een script dat checkt of het rapportcijfer groter is dan 8.
+//     Als dat zo is, loggen we 'Cum laude!' in de console.                // geeft 'Cum laude!' wanneer gelogd
+// ==========================================
+
+const grade = 9;
+const cumLaude = grade > 8;
+
+if (cumLaude) {
+    console.log("Cum laude!");
+}
+else {
+    console.log("Helaas, geen cum laude.");
+}
+
+// ==========================================
+// 5a. Gebruik onderstaande variabelen isRaining en temperature;
+// 5b. Schrijf een script dat "Je kunt een korte broek aan!" logt wanneer het niet regent EN de temperatuur hoger is dan 21 graden
+// 5c. Log "Doe een jas aan!" Als het regent OF kouder is dan 12 graden
+// Tip: verander de waarde van isRaining en temperature even handmatig om te kijken of jouw 2e conditie klopt
+// ==========================================
+
+const isRaining = false;
+const temperature = 12;
+
+if (!isRaining && temperature > 21) {
+    console.log("Je kunt een korte broek aan!");
+}
+
+if (isRaining || temperature < 12) {
+    console.log("Doe een jas aan!");
+}
