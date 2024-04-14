ec48b04acac12fe1c1b33ca5a9e7bcf24acc4005
AnneKluytmans <annekluytmans1@gmail.com>
2024-04-14 13:04:04 +0200

    Finished methods assignments edhub h5

---
+++ frontend-javascript-methods-h5/date-methods.js
@@ -0,0 +1,33 @@
+/* Opdracht 0: voorbeeld */
+// Maak een nieuw date-object aan voor dit huidige moment en log dit in de terminal
+// ---- Verwachte uitkomst: 2021-04-06T14:54:34.062Z (afhankelijk van het moment)
+
+const currentDay = new Date();
+console.log(currentDay);
+
+
+/* Opdracht 1 */
+// 1a. Maak een nieuw date-object aan voor dit huidige moment en log het aantal uren in de terminal
+// 1b. Log daarna het aantal minuten in de terminal
+// ---- Verwachte uitkomsten:
+// Uren: 16 (als er een vier in de klok zit op het moment van aanroepen)
+// Minuten: 36 (als het 16:36 is op het moment van aanroepen)
+
+const currentHours = currentDay.getHours();
+const currentMinutes = currentDay.getMinutes();
+
+console.log(currentHours);
+console.log(currentMinutes);
+
+/* Opdracht 2 */
+// Format het date-object dat je bij de vorige vraag hebt gemaakt naar een leesbare datum
+// ---- Verwachte uitkomst: Tue Apr 06 2021 (afhankelijk van het moment)
+
+const writeOptions = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+}
+const currentDateToString = currentDay.toLocaleDateString('NL-nl', writeOptions);
+console.log(currentDateToString);
---
+++ frontend-javascript-methods-h5/string-methods.js
@@ -0,0 +1,70 @@
+/* Opdracht 0: voorbeeld */
+// Check de lengte van onderstaande string en log dit in de terminal
+// ---- Verwachte uitkomst: 10
+
+const word = "zoetsappig";
+console.log(word.length);
+
+
+/* Opdracht 1 */
+//  Check of het woord "Banaan" langer is dan het woord "Aardappel" en log dit in de terminal.
+// ---- Verwachte uitkomst: false
+
+const fruit = "Banaan";
+const vegetable = "Aardappel";
+
+const compareWordLength = fruit.length > vegetable.length;
+console.log(compareWordLength);
+
+/* Opdracht 2 */
+//  Zorg ervoor dat de laatste letter van het woord in fruit en vegetable (hierboven) in de terminal gelogd wordt.
+//  Tip: bedenk 1 manier die voor beide woorden zou werken.
+// ---- Verwachte uitkomst voor fruit: "n"
+// ---- Verwachte uitkomst voor vegetable: "l"
+
+function giveLastLetter(word) {
+    const lastLetter= word.charAt(word.length - 1);
+    return lastLetter;
+}
+
+console.log(giveLastLetter(fruit));
+console.log(giveLastLetter(vegetable));
+
+/* Opdracht 3 */
+//  Haal de spaties aan het begin en eind van onderstaande zin weg en log dit in de terminal
+// ---- Verwachte uitkomst: "De kat krabt de krullen van de trap"
+
+const userInput = "  De kat krabt de krullen van de trap ";
+
+const trimmedUserInput = userInput.trim();
+console.log(trimmedUserInput);
+
+/* Opdracht 4 */
+// Check of het woord "Fantastisch" in onderstaande zin voorkomt en log dit in de terminal.
+// ---- Verwachte uitkomst: true
+
+const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch, nietwaar?"
+
+const includesWord = story.includes("Fantastisch");
+console.log(includesWord);
+
+/* Opdracht 5 */
+// Knip de vraag van bovenstaande tekst (story) af, zodat alleen het eerste gedeelte overblijft en log dit in de terminal.
+// ---- Verwachte uitkomst: "We vinden de behaalde resultaten werkelijk prachtig."
+
+const substringEnd = story.indexOf(".") + 1;
+const storySentence = story.substring(0, substringEnd);
+
+console.log(storySentence);
+
+/* Opdracht 6 */
+// Maak van onderstaand bedrag een Nederlands bedrag door het volgende te doen:
+// * Vervang het dollar-teken voor een euro-teken
+// * Vervang de punt voor een komma
+// Log dit in de terminal.
+// ---- Verwachte uitkomst: "€35,59"
+
+const price = "$35.59";
+
+const dutchPrice = price.replaceAll("$", "€").replaceAll(".", ",");
+console.log(dutchPrice);
